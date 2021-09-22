1e63673ad5c6ff2c74f88df579984b1101b2d818
Susumu Yamazaki <zacky1972@gmail.com>
2021-09-23 08:39:31 +0900

    version 0.1.2

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NxNif.MixProject do
   def project do
     [
       app: :nx_nif,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
