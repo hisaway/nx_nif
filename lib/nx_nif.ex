530a46771bfe8ccd55f45dcb6b02a7fcf76d4562
Susumu Yamazaki <zacky1972@gmail.com>
2021-09-23 04:04:19 +0900

    describe libnif.c to implement temporal test function

--- lib/nx_nif.ex
+++ lib/nx_nif.ex
@@ -1,7 +1,23 @@
 defmodule NxNif do
+  require Logger
+
   @moduledoc """
   Documentation for `NxNif`.
   """
+
+  @on_load :load_nif
+
+  def load_nif do
+    nif_file = 'priv/libnif'
+
+    case :erlang.load_nif(nif_file, 0) do
+      :ok -> :ok
+      {:error, {:reload, _}} -> :ok
+      {:error, reason} -> Logger.error("Failed to load NIF: #{inspect(reason)}")
+    end
+  end
+
+  def test(), do: raise("NIF test/0 not implemented")
 
   @doc """
   Hello world.
