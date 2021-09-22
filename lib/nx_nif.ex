7fb24bf3d42fc51a94663fdbd1ab30353703008b
Susumu Yamazaki <zacky1972@gmail.com>
2021-09-23 03:12:35 +0900

    initial commit

---
+++ lib/nx_nif.ex
@@ -0,0 +1,18 @@
+defmodule NxNif do
+  @moduledoc """
+  Documentation for `NxNif`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NxNif.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
