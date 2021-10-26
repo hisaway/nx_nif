c327644ecc56cc9304b3ddb2a551a7f31a089b73
Susumu Yamazaki <zacky1972@gmail.com>
2021-10-27 04:01:22 +0900

    version 0.2.0 and update mix.lock

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule NxNif.MixProject do
   def project do
     [
       app: :nx_nif,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
